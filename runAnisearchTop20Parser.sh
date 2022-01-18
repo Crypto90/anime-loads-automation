7fce29d53d3c951cc806db4871a80613745ca653
Crypto90 <support@orangesiri.com>
2022-01-18 12:18:35 +0100

    Added conjob sh files.

---
+++ runAnisearchTop20Parser.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#first cd to this current script dir
+cd "$(dirname "$0")"
+
+python3 parseAniSearchPopular20.py >> parseAniSearchPopular20.log
---
+++ runParseRequestedAnimeMoviesAndSeries.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#first cd to this current script dir
+cd "$(dirname "$0")"
+
+python3 parseRequestedAnimeMoviesAndSeries.py >> parseRequestedAnimeMoviesAndSeries.log
